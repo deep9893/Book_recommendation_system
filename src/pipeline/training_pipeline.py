0eec741b6ef6a3031fa68eaf2893e29ee37e654e
deep9893 <deeppratapsingh0996@gmail.com>
2023-08-26 14:47:47 +0530

    model training  completed

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,14 +1,14 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
-# from src.components.model_training import ModelTrainer
+from src.components.model_training import ModelTrainer
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
         self.data_transformation = DataTransformation()
-        # self.model_training = ModelTrainer()
+        self.model_training = ModelTrainer()
 
     def start_training_pipeline(self):
         """
@@ -18,5 +18,5 @@ class TrainingPipeline:
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
         self.data_transformation.initiate_data_transformation()
-        # self.model_training.initiate_model_trainer()
+        self.model_training.initiate_model_trainer()
 
